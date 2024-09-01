05d3347cb0021d34e8ac2283e0e39f2bf8b4fec5
Zeeshan Habib <zeeshanhabib21@gmail.com>
2024-09-01 23:24:57 +0500

    render project with pagination

--- laravel11-react-intertia/database/seeders/DatabaseSeeder.php
+++ laravel11-react-intertia/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
+use App\Models\Team;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Project;
+use App\Models\TeamUser;
+use App\Models\ActivityLog;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +19,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create Users
+        User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create Teams
+        Team::factory(5)->create()->each(function ($team) {
+            $team->users()->attach(User::inRandomOrder()->take(3)->pluck('id'));
+        });
+
+        // Seed data for TeamUser pivot table
+        TeamUser::factory()->count(50)->create();
+
+        // Create Projects
+        Project::factory(5)->create();
+
+        // Create Tasks
+        Task::factory(20)->create();
+
+        // Create Comments
+        Comment::factory(50)->create();
+
+        // Create Activity Logs
+        ActivityLog::factory(50)->create();
     }
 }
