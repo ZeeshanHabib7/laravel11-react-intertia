8285336839547abc85d203af9710749a55cb3416
DESKTOP-20UIB3O\Dell <zeeshan.habib@invisionsolutions.ca>
2024-09-05 20:02:17 +0500

    project table render with pagination

---
+++ laravel11-react-intertia/app/Http/Resources/ProjectResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\OwnerResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
+            "owner" => new OwnerResource($this->owner), // Use new for a single instance
+            "start_date" => $this->start_date,
+            "end_date" => $this->end_date,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+            'deleted_at' => $this->deleted_at ? $this->deleted_at->toDateTimeString() : null,
+        ];
+    }
+}
